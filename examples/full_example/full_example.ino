38aa5d1e6d4668cb1767a523051d4b17b8b9f888
nthnn <nathanneisip@gmail.com>
2023-08-31 01:27:20 +0800

    Working example.

--- examples/full_example/full_example.ino
+++ examples/full_example/full_example.ino
@@ -1,8 +1,6 @@
 #include <planet096.h>
 
 void setup() {
-    auto display = Planet096App::getI2CScreen();
-
     auto scene = Planet096Scene("Hello");
     scene.setAppBarAlignment(APPBAR_ALIGN_CENTER);
     scene.setAppBarStyle(APPBAR_NORMAL);
