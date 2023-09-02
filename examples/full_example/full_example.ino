40a827a8c11037a30ee9e6ba0e0e5ace0e49986f
nthnn <nathanneisip@gmail.com>
2023-09-02 22:03:53 +0800

    Updated full example sketch.

--- examples/full_example/full_example.ino
+++ examples/full_example/full_example.ino
@@ -4,11 +4,13 @@ void setup() {
     Planet096Scene scene("Example");
     scene.setAppBarAlignment(PLANET096_APPBAR_ALIGN_CENTER);
     scene.setAppBarStyle(PLANET096_APPBAR_NORMAL);
+
+    scene.setSceneMenuStyle(PLANET096_SCENE_MENU_BUTTONS);
     scene.setSceneMenu("Back", "Set", "Ok");
 
     Planet096Text text(&scene, "Hello, world!");
     text.setX(26);
-    text.setY(28);
+    text.setY(14);
     text.setTextSize(1);
 
     scene.setMainWidget(createWidget(&text));
