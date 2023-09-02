ca6c6aa8caab16951c2c3fed9acbd09e59bbf025
nthnn <nathanneisip@gmail.com>
2023-09-02 23:58:37 +0800

    Header file for color definitions.

--- src/planet096.h
+++ src/planet096.h
@@ -2,6 +2,7 @@
 #define PLANET096_H
 
 #include <planet096_app.h>
+#include <planet096_colors.h>
 #include <planet096_scene.h>
 #include <planet096_ui_text.h>
 #include <planet096_widget.h>
---
+++ src/planet096_colors.h
@@ -0,0 +1,9 @@
+#ifndef PLANET096_DEFS_H
+#define PLANET096_DEFS_H
+
+enum Planet096Color {
+    PLANET096_BLACK,
+    PLANET096_WHITE
+};
+
+#endif
