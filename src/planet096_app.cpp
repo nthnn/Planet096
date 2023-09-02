bc8e1e13a87c14aaf0ab228ed68670bd252f8d03
nthnn <nathanneisip@gmail.com>
2023-09-02 23:58:04 +0800

    Fixed display initialization.

--- src/planet096_app.cpp
+++ src/planet096_app.cpp
@@ -4,9 +4,9 @@
 #include "planet096_app.h"
 #include "planet096_scene.h"
 
-Adafruit_SSD1306 Planet096App::getI2CScreen() {
-    Adafruit_SSD1306 display(128, 64, &Wire, -1);
+static Adafruit_SSD1306 display(128, 64, &Wire, -1);
 
+Adafruit_SSD1306 Planet096App::getI2CScreen() {
     if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
         return NULL;
 
