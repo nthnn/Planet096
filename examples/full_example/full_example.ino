5f637bdad9551ffab605604ec6e8bb1dc49b8200
nthnn <nathanneisip@gmail.com>
2023-09-02 15:11:08 +0800

    Updated examples.

--- examples/full_example/full_example.ino
+++ examples/full_example/full_example.ino
@@ -1,11 +1,17 @@
 #include <planet096.h>
 
 void setup() {
-    Planet096Scene scene("Hello");
-    scene.setAppBarAlignment(P96_APPBAR_ALIGN_CENTER);
-    scene.setAppBarStyle(P96_APPBAR_NORMAL);
+    Planet096Scene scene("Example");
+    scene.setAppBarAlignment(PLANET096_APPBAR_ALIGN_CENTER);
+    scene.setAppBarStyle(PLANET096_APPBAR_NORMAL);
     scene.setSceneMenu("Back", "Set", "Ok");
 
+    Planet096Text text(&scene, "Hello, world!");
+    text.setX(26);
+    text.setY(28);
+    text.setTextSize(1);
+
+    scene.setMainWidget(createWidget(&text));
     Planet096App::start(scene);
 }
 
