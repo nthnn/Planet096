7069be78197ca28b83c7097b3e4a19cd65ad0979
nthnn <nathanneisip@gmail.com>
2023-09-03 23:48:10 +0800

    Planet096 widget structure.

--- src/planet096_widget.h
+++ src/planet096_widget.h
@@ -1,13 +1,16 @@
 #ifndef PLANET096_WIDGET_H
 #define PLANET096_WIDGET_H
 
+#include <planet096_ui_scrollable_text.h>
 #include <planet096_ui_text.h>
 
-#define PLANET096_WUI_NONE 0x00
-#define PLANET096_WUI_TEXT 0x01
+#define PLANET096_WUI_NONE              0x00
+#define PLANET096_WUI_TEXT              0x01
+#define PLANET096_WUI_SCROLLABLE_TEXT   0x02
 
 typedef struct _Planet096Widget {
     uint8_t widget_type;
+    Planet096ScrollableText* scrollable_text_ui;
     Planet096Text* text_ui;
 
     _Planet096Widget() {
@@ -22,6 +25,13 @@ typedef struct _Planet096Widget {
 static Planet096Widget createWidget(Planet096Text* textUI) {
     Planet096Widget widget(PLANET096_WUI_TEXT);
     widget.text_ui = textUI;
+
+    return widget;
+}
+
+static Planet096Widget createWidget(Planet096ScrollableText* scrollableTextUI) {
+    Planet096Widget widget(PLANET096_WUI_SCROLLABLE_TEXT);
+    widget.scrollable_text_ui = scrollableTextUI;
 
     return widget;
 }
