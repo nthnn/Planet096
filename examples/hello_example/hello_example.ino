f08d9f138b5f37c609611ef1bd4135f69596e959
nthnn <nathanneisip@gmail.com>
2023-09-04 00:23:50 +0800

    Renamed to 'hello_example'

---
+++ examples/hello_example/hello_example.ino
@@ -0,0 +1,18 @@
+#include <planet096.h>
+
+void app() {
+    Planet096Scene scene("Example");
+    scene.setAppBarAlignment(PLANET096_APPBAR_ALIGN_CENTER);
+    scene.setAppBarStyle(PLANET096_APPBAR_NORMAL);
+
+    scene.setSceneMenuStyle(PLANET096_SCENE_MENU_BUTTONS);
+    scene.setSceneMenu("Back", "Set", "Ok");
+
+    Planet096Text text(&scene, "Hello, world!");
+    text.setX(26);
+    text.setY(14);
+    text.setTextSize(1);
+
+    scene.setMainWidget(createWidget(&text));
+    Planet096App::start(scene);
+}
